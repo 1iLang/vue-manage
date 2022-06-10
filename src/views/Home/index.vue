7c24565d3ae9017f36fcf4dc1a4842fe272a421e
lilang <995914589@qq.com>
2022-06-10 16:51:25 +0800

    update

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -197,5 +197,14 @@ export default {
       ],
     };
   },
+  mounted() {
+    this.$http.get('/user?ID=12345')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 };
 </script>
